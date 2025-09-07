35e2b3e94f056fc6165d0eb49193ede97293c9e0
Kartik Papney <pdt.kartik@gmail.com>
2025-09-07 17:45:16 +0530

    feat: algorithms

--- index.js
+++ index.js
@@ -37,6 +37,23 @@ app.use('/playground', createProxyMiddleware({
   }
 }));
 
+app.use('/algorithms', createProxyMiddleware({
+  target: `http://127.0.0.1:${process.env.ALGORITHMS}`,
+  changeOrigin: true,
+  preserveHeaderKeyCase: true,
+  followRedirects: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // Ensure request body is properly forwarded
+    if (req.body && Object.keys(req.body).length > 0) {
+      const bodyData = JSON.stringify(req.body);
+      proxyReq.setHeader('Content-Type', 'application/json');
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
+  }
+}));
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to the reverse proxy server!');
 });
